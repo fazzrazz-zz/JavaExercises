3d4ac33fc22f13c9b1c5c4163fb14fae83e8a992
Dan Duran <danielduranrojas@gmail.com>
2019-02-02 18:54:42 -0500

    Add files via upload

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Dan!");
+
+        int numberVar1 = 5;
+        int numberVar2 = 10;
+        int numberVar3 = 15;
+
+        int numberTotal = numberVar1 + numberVar2 + numberVar3;
+        int totalThousand = 1000 - numberTotal;
+
+        System.out.println(totalThousand);
+    }
+}
