fb2e3bd75b9312adff67146cf9e5cf211a5b6ea8
Dan Duran <danielduranrojas@gmail.com>
2019-02-10 22:59:36 -0500

    Add files via upload

---
+++ StatementsWhitespaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVar = 50;
+        myVar++;
+        myVar--;
+        System.out.println("Test");
+
+        System.out.println("This is still " +
+            "the same " +
+            "line");
+
+        int anothervariable=50;myVar++;System.out.println("Another one");
+    }
+}
